5e7881473a79ea19aac1b97ac4afb2efcf7a1b1a
Zain Ul-Abdeen <ieulabdeen.zain@gmail.com>
2022-06-25 02:22:03 +0100

    Added responsivness and styling to the navbar TODO:Personal links in mobile nav bar need to be added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
@@ -25,10 +26,48 @@ const Navbar = () => {
                         <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                     </Link>
                 </ul>
+                <div className='md:hidden'>
+                    <AiOutlineMenu size={25} />
+                </div>
+            </div>
+        </div>
+
+        <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+            <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+                <div>
+                    <div className='flex w-full items-center justify-between'>
+                        <Image src='/../public/assets/navLogo.png' width='87' height='35' alt='Portfolio Logo written in green with a white background, Zain Ul-Abdeen, Mobile Nav Menu' />
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'>
+                        <p className='w-[85%] md:w-[90%] py-4'>I Love To Bring Websites To Life!</p>
+                    </div>
+                </div>
+                <div className='py-4 flex flex-col'>
+                    <ul className='uppercase'>
+                        <Link href='/'>
+                        <li className='py-4 text-sm'>Home</li>
+                        </Link>
+                        <Link href='/'>
+                        <li className='py-4 text-sm'>About</li>
+                        </Link>
+                        <Link href='/'>
+                        <li className='py-4 text-sm'>Skills</li>
+                        </Link>
+                        <Link href='/'>
+                        <li className='py-4 text-sm'>Projects</li>
+                        </Link>
+                        <Link href='/'>
+                        <li className='py-4 text-sm'>Contact</li>
+                        </Link>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
