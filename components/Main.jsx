a74df6a3dad595de5b497ca0e11005f1eb53ee75
Zain Ul-Abdeen <ieulabdeen.zain@gmail.com>
2022-06-27 23:58:11 +0100

    Completed Navbar component, added headers, text and LinkedIn icon to Main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { FaLinkedinIn } from 'react-icons/fa'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='w-full h-screen text-center'>
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className='uppercase text-sm tracking-widest text-gray-600'>I LOVE TO BRING WEBSITES TO LIFE!</p>
+          <h1 className='py-4 text-gray-700'>Hello, I'm <span className='text-[#5651e5]'>Zain</span></h1>
+          <h1 className='py-2 text-gray-700'>I'm A Passionate Software Developer</h1>
+          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat quae sequi adipisci et labore. Maiores laboriosam minus eum fugit modi excepturi, quaerat temporibus dolores ab consequuntur, omnis possimus at pariatur.</p>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in-out duration-300'>
+              <FaLinkedinIn />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
