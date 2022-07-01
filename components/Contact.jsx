2ef50529f0d0c29d981b11465d4ce8387c241d36
Zain Ul-Abdeen <ieulabdeen.zain@gmail.com>
2022-07-01 21:11:26 +0100

    Finished left sided contact card TODO:work on right side of contact section

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,21 +1,65 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonFill } from "react-icons/bs";
+import {
+  FaGithub,
+  FaHackerrank,
+  FaLinkedinIn,
+  FaStackOverflow,
+} from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
-          Contact
+          Contact Me
         </p>
-        <h2 className="py-4">Let's Get In Touch!</h2>
+        <h2 className="py-4">Let's Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
+          {/* left */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full">
               <div>
-                <img src="" alt="/" />
+                <img
+                  className="rounded-xl hover:scale-105 ease-in duration-300"
+                  src="https://source.unsplash.com/npxXWgQ33ZQ"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h2 className="py-2">Zain Ul-Abdeen</h2>
+                <p>Software Developer</p>
+                <p className="py-4">
+                  I am looking for full-time roles. Let's Talk!
+                </p>
+              </div>
+              <div>
+                <p className="uppercase pt-8">Connect With Me</p>
+                <div className="flex items-center justify-between py-4">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in-out duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in-out duration-300">
+                    <FaGithub />
+                  </div>
+                  {/* <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in-out duration-300'>
+              <FaStackOverflow />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in-out duration-300'>
+              <FaHackerrank />
+            </div> */}
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in-out duration-300">
+                    <AiOutlineMail />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in-out duration-300">
+                    <BsFillPersonFill />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          {/* right */}
         </div>
       </div>
     </div>
