c1685491811a1aad9a32dd3bd1b6a04a0541113f
Zain Ul-Abdeen <ieulabdeen.zain@gmail.com>
2022-07-01 18:25:00 +0100

    Created Contact component and Contact card //INCOMPLETE

---
+++ components/Contact.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Contact
+        </p>
+        <h2 className="py-4">Let's Get In Touch!</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full">
+              <div>
+                <img src="" alt="/" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
