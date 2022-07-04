c3e147554ae436a1fabde156c0cba49a38f775dd
Zain Ul-Abdeen <ieulabdeen.zain@gmail.com>
2022-07-05 00:29:40 +0100

    Reverting to (27838f2) because contact form functionality was unsuccessful

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import { useForm } from "react-hook-form";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import {
@@ -12,17 +11,6 @@ import {
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 const Contact = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm();
-  console.log(errors);
-  function onSubmitForm(values) {
-    console.log(values);
-  }
   return (
     <div id="contact" className="w-full lg:h-screen bg-green-white-0">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
@@ -73,29 +61,16 @@ const Contact = () => {
               </div>
             </div>
           </div>
-
-          {/* right  Contact Form  */}
+          {/* right */}
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form onSubmit={handleSubmit(onSubmitForm)}>
+              <form>
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
-                    <label
-                      for="name"
-                      name="name"
-                      className="uppercase text-sm py-2"
-                    >
-                      Name
-                    </label>
+                    <label className="uppercase text-sm py-2">Name</label>
                     <input
-                      className={`border-2 rounded-lg p-3 flex border-gray-300 w-full shadow px-4 placeholder-gray-500 focus:ring-green-500 focus:border-green-500 focus:outline-none focus:ring-2 ${
-                        errors.name ? "ring-2 ring-red-500" : null
-                      }`}
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
-                      {...register("name", {
-                        required: true,
-                        message: "You must enter your name",
-                      })}
                     />
                   </div>
                   <div className="flex flex-col">
@@ -105,90 +80,28 @@ const Contact = () => {
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
-                      {...register("phone", {
-                        required: false,
-                        minLength: {
-                          value: 10,
-                          message: "Phone number too short",
-                        },
-                        maxLength: {
-                          value: 14,
-                          message: "Phone number too long",
-                        },
-                      })}
                     />
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label for="email" className="uppercase text-sm py-2">
-                    Email
-                  </label>
+                  <label className="uppercase text-sm py-2">Email</label>
                   <input
-                    className={`border-2 rounded-lg p-3 flex border-gray-300 w-full shadow px-4 placeholder-gray-500 focus:ring-green-500 focus:border-green-500 focus:outline-none focus:ring-2 ${
-                      errors.email ? "ring-2 ring-red-500" : null
-                    }`}
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="email"
-                    {...register("email", {
-                      required: true,
-                      message: "You must enter your email address",
-                      minLength: {
-                        value: 8,
-                        message: "This is not long enough to be an email",
-                      },
-                      maxLength: {
-                        value: 120,
-                        message: "This is too long to be an email ",
-                      },
-                      pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: "This needs to be a valid email",
-                      },
-                    })}
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label for="subject" className="uppercase text-sm py-2">
-                    Subject
-                  </label>
+                  <label className="uppercase text-sm py-2">Subject</label>
                   <input
-                    className={`border-2 rounded-lg p-3 flex border-gray-300 w-full shadow-px-4 placeholder-gray-500 focus:ring-green-500 focus:border-green-500 focus:outline-none focus:ring-2 ${
-                      errors.subject ? "ring-2 ring-red-500" : null
-                    }`}
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
-                    {...register("subject", {
-                      required: true,
-                      message: "You must enter a subject",
-                      minLength: {
-                        value: 1,
-                        message: "Subject length is too short",
-                      },
-                      maxLength: {
-                        value: 120,
-                        message: "Subject length is too long",
-                      },
-                    })}
                   />
                 </div>
                 <div className="flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Message</label>
                   <textarea
-                    className={`border-2 rounded-lg p-3 border-gray-300 w-full shadow-px-4 placeholder-gray-500 focus:ring-green-500 focus:border-green-500 focus:outline-none focus:ring-2 ${
-                      errors.message ? "ring-2 ring-red-500" : null
-                    }`}
+                    className="border-2 rounded-lg p-3 border-gray-300"
                     rows="10"
-                    {...register("message", {
-                      required: true,
-                      message: "You must enter a message",
-                      maxLength: {
-                        value: 1000,
-                        message: "Messages cant be more than 1000 characters",
-                      },
-                      minLength: {
-                        value: 50,
-                        message:
-                          "Messages need to be at least 50 characters long",
-                      },
-                    })}
                   ></textarea>
                 </div>
                 <button className="w-full p-4 text-gray-100 mt-4 cursor-pointer hover:scale-105 ease-in duration-300 bg-green-white-5">
